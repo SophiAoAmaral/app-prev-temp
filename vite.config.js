41f1adc4c2ee991fe77a097d82afbfac5216f3b2
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-11-18 19:03:56 -0300

    deploy fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/app-prev-temp/",
   plugins: [react()],
-  base: '/app-prev-temp/',
-})
+});
